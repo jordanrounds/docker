7d30e53046f196fc2df73346a60c6db3b66dc401
Jordan Rounds <jrounds@mac.com>
2026-02-21 16:38:19 -0800

    Remove unused services: heimdall, jackett, mariadb, nginx, syncthing

--- heimdall/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo
-echo "Heimdall Container Init"
-echo
-echo "Creating link to Heimdall data"
-ln -sf $LOCAL_PERSIST/heimdall persist-data
--- syncthing/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source '../functions.sh'
-
-start_container 'Syncthing'
-cleanup_old_images
